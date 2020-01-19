93059b438647eef973a0660882aa37437e1b7a78
reinralle <reinralle@gmail.com>
2020-01-19 07:54:31 +0100

    add nvm example

---
+++ Development/test_nvm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear; echo
+
+source ~/.nvm/nvm.sh # . ~/.nvm/nvm.sh
+
+# printf "%s\n\n" "$(bash -c "help")"
+# printf "%s\n\n" "$(bash "--help")"
+# printf "%s\n\n" "$(bash -c "help set")"
+
+printf "%s\n\n" "$(nvm --help)"
+# printf "%s\n\n" "$(nvm --version)"
+# printf "%s\n\n" "$(nvm debug)"
+# printf "%s\n\n" "$(nvm ls)"
+# printf "%s\n\n" "$(nvm exec v12.14.0 node --version)"
+# printf "%s\n\n" "$(nvm exec v12.14.0 node --help)"
+# printf "%s\n\n" "$(nvm exec v12.14.0 node --v8-options)"
+
+# node --version
