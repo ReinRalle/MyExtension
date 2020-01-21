288e26e9e00ffeaa2c925806a7c3596487c8c6d8
reinralle <reinralle@gmail.com>
2020-01-21 16:42:56 +0100

    :lipstick: , add some "whiches"

--- .github/scripts/Diagnose.sh
+++ .github/scripts/Diagnose.sh
@@ -2,7 +2,7 @@
 
 # chmod +x Diagnose.sh
 
-clear
+
 
 TRENNLINIE="--------------------------------------------------------------"
 
@@ -19,14 +19,21 @@ printout(){ # $1=heading, $2=string, $3=enclose (0 = no enclose, 1 = enclose in 
 
 }
 
-printout "Get current date and time"      "$(date +'%d.%m.%y - %H:%Mh')" 1
+clear
 
-printout "list root directory"            "$(ls -lAv --color=always --group-directories-first /)" 0
-printout "list home directory"            "$(ls -lAv --color=always --group-directories-first ~)" 0
-printout "list current directory"         "$(ls -lAv --color=always --group-directories-first)" 0
-printout "list script directory"          "$(ls -lAv --color=always --group-directories-first ./.github/scripts/)" 0
-printout "list '/bin' directory"          "$(ls -lAv --color=always --group-directories-first /bin)" 0
-printout "list '/usr/bin' directory"      "$(ls -lAv --color=always --group-directories-first /usr/bin)" 0
+echo
+
+printout "Get current date and time"           "$(date +'%d.%m.%y - %H:%Mh')" 1
+
+printout "list root directory"                 "$(ls -lAv --color=always --group-directories-first /)" 0
+printout "list home directory"                 "$(ls -lAv --color=always --group-directories-first ~)" 0
+printout "list current directory"              "$(ls -lAv --color=always --group-directories-first)" 0
+printout "list script directory"               "$(ls -lAv --color=always --group-directories-first ./.github/scripts/)" 0
+printout "list '/bin' directory"               "$(ls -lAv --color=always --group-directories-first /bin)" 0
+printout "list '/usr/bin' directory"           "$(ls -lAv --color=always --group-directories-first /usr/bin)" 0
+printout "list '/home/runner/work' directory"  "$(ls -lAv --color=always --group-directories-first /home/runner/work)" 0
+
+/home/runner/work
 
 printout "show set"                       "$(set)" 0
 
@@ -34,3 +41,7 @@ printout "show git status"                "$(git status)" 0
 
 printout "list local installed packages"  "$(npm ll --depth 0)" 0
 printout "list global installed packages" "$(npm ls -g --depth 0)" 0
+
+which awk
+which perl
+which sed
