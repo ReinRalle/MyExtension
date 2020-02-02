16cbbe073400170ae741466d35a9cd33af344541
reinralle <reinralle@gmail.com>
2020-02-02 16:07:28 +0100

    expand html

--- src/html/app.js
+++ src/html/app.js
@@ -1,30 +1,19 @@
-
-function print_console(): void {
-
+"use strict";
+function print_console() {
     console.log("start app.js");
     console.warn("show warnings...");
     console.error("show errors...");
-
     console.error("show errors..");
     console.error("show errors........");
-
 }
 debugger;
-
-// print_console();
-
 try {
-
     var doc = document.getElementById("demo");
-
     if (doc !== null) {
         doc.innerHTML = "5 + 16";
     }
-
-} catch (error) {
+}
+catch (error) {
     console.error(error);
 }
-
-
-
-
+//# sourceMappingURL=app.js.map
